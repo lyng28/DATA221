b2519b1cd8b5e5074ee5e2bf37432e42bc075ea6
lyng28 <thuly2000@gmail.com>
2026-01-28 16:19:27 -0700

    Add solution for Assignment 1 Question 3 '

---
+++ DATA221_Assignments/DATA221_Assignment1_Question3.py
@@ -0,0 +1,14 @@
+# Safe function Application
+
+def power (x, y):
+    return x ** y
+
+pairs = [[5, 2], [3, -1], [4, 3], [2, 0]]
+result = []
+
+for x, y in pairs: #unpacking argument
+    if y < 0:
+        continue
+    result.append(power(x, y))
+
+print(result)

0c2aea5491f7c83635d20ade2a6dd252f94675aa
lyng28 <thuly2000@gmail.com>
2026-01-28 16:18:27 -0700

    Add solution for Assignment 1 Question 2

---
+++ DATA221_Assignments/DATA221_Assignment1_Question2.py
@@ -0,0 +1,18 @@
+# Nested Dictionaries from Strings
+
+def Nested_Dictionaries_from_Strings(list_of_strings):
+    nested_dictionary = {}
+    for word in list_of_strings:
+        word_length = len(word)
+        if word_length % 2 == 0:
+            word_length_parity = "even"
+        else:
+            word_length_parity = "odd"
+        nested_dictionary[word] = {
+            "length": word_length,
+            "parity": word_length_parity,
+        }
+    return nested_dictionary
+
+example_strings = ["data", "science"]
+print(Nested_Dictionaries_from_Strings(example_strings))
